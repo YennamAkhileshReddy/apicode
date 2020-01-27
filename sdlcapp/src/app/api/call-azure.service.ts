a289fcf89be4119439c89106bafe2fc9e878b9a9
orgd <gcshar@yahoo.com>
2020-01-27 11:54:42 -0500

    added PAT to call the azure servie

--- sdlcapp/src/app/api/call-azure.service.ts
+++ sdlcapp/src/app/api/call-azure.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import { HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,11 @@ export class CallAzureService {
 
   GetAzureDetails()
   {
-    return this.http.get("https://dev.azure.com/GIRISHSHARMA0640/DevOpsSM/_apis/build/builds?api-version=5.1");
+    let headers: HttpHeaders = new HttpHeaders();
+    headers.append('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+    headers.append('Authorization', "2xbpth7m4xi34hx6wawwpbyfvllscthvfgiowukmngbaat3lddwa");
+
+    return this.http.get("https://dev.azure.com/GIRISHSHARMA0640/DevOpsSM/_apis/build/builds?api-version=5.1", {headers});
                           
 //https://dev.azure.com/GIRISHSHARMA0640/DevOpsSM/_apis/build/builds?api-version=5.1
   }
